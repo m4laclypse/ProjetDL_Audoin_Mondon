ba93e87adab63957dcc9bfb6e5fef74fcd98ae83
Malaclypse Félix <45031721+m4laclypse@users.noreply.github.com>
2021-02-14 15:54:00 +0100

    Update flappy.py

--- flappy.py
+++ flappy.py
@@ -5,24 +5,15 @@ Created on Sun Feb 14 13:58:18 2021
 @author: loic9
 """
 
-import pygame, sys, math
+import pygame
+import sys
+import math
 from random import randrange as randH
+
+from pygame.constants import KEYDOWN, K_ESCAPE, QUIT
 from pygame.locals import *
 
-# Flappy python? by John Mathison
-# Checking to see how long it takes to make flappy bird
-
-# Start pygame
-pygame.init()
-
-
-import pygame, sys, math
-from random import randrange as randH
-from pygame.locals import *
-
-# Set up resolution
-windowObj = pygame.display.set_mode( ( 640, 480) )
-fpsTimer = pygame.time.Clock()
+# by John Mathison
 
 # Ground Elevation (pixels)
 groundLevel = 400
@@ -33,9 +24,6 @@ backgroundColor = pygame.Color('#abcdef')
 groundColor = pygame.Color('#993333')
 fontColor = pygame.Color('#FFFFFF')
 
-# fontObj = pygame.font.Font('OpenSans-Bold.ttf', 16)
-fontObj = pygame.font.Font(None, 16)
-   
 
 # Class for pipe obstacles
 class Pipes:
@@ -43,7 +31,7 @@ class Pipes:
     height = 0
     width = 60
     gap = 150
-    pos = 600;
+    pos = 600
     replaced = False
     scored = False
 
@@ -54,28 +42,29 @@ class Pipes:
     # Moves the pipes along the ground, checks if they're off the screen
     def move(self, movement):
         self.pos += movement
-        if( self.pos + self.width < 0 ):
-            return False #Return false if we moved off the screen 
+        if (self.pos + self.width < 0):
+            return False  # Return false if we moved off the screen
         return True
 
     # Handles drawing the pipes to the screen
     def draw(self, surface):
-        pygame.draw.rect( surface, groundColor, (self.pos, self.height, self.width, groundLevel - self.height))
-        pygame.draw.rect( surface, groundColor, (self.pos, 0, self.width, self.height - self.gap))
+        pygame.draw.rect(surface, groundColor, (self.pos, self.height, self.width, groundLevel - self.height))
+        pygame.draw.rect(surface, groundColor, (self.pos, 0, self.width, self.height - self.gap))
 
-# Class for the player 
+
+# Class for the player
 class Bird:
-    
-    pos = (0,0)
+
+    pos = (0, 0)
     radius = 20
-    
+
     def __init__(self, newPos):
         self.pos = newPos
 
     # Handles drawing the bird to the screen
     def draw(self, surface):
-        intPos = ( int(math.floor(self.pos[0])), int(math.floor(self.pos[1])) )
-        
+        intPos = (int(math.floor(self.pos[0])), int(math.floor(self.pos[1])))
+
         pygame.draw.circle(surface, birdColor, intPos, self.radius)
 
     # Attempt to move the bird, make sure we aren't hitting the ground
@@ -83,19 +72,19 @@ class Bird:
         posX, posY = self.pos
         movX, movY = movement
 
-        if( (posY + movY + self.radius) < groundLevel ):
+        if((posY + movY + self.radius) < groundLevel):
             self.pos = (posX + movX, posY + movY)
-            return True #Return if we successfuly moved
+            return True  # Return if we successfuly moved
         self.pos = (posX, groundLevel - self.radius)
         return False
 
     # Test for collision with the given pipe
     def collision(self, pipe):
         posX, posY = self.pos
-        collideWidth = ( pipe.pos < posX + self.radius and posX - self.radius < pipe.pos + pipe.width)
-        collideTop = ( pipe.height - pipe.gap > posY - self.radius )
-        collideBottom = ( posY + self.radius > pipe.height )
-        if (  collideWidth and ( collideTop or collideBottom)):
+        collideWidth = (pipe.pos < posX + self.radius and posX - self.radius < pipe.pos + pipe.width)
+        collideTop = (pipe.height - pipe.gap > posY - self.radius)
+        collideBottom = (posY + self.radius > pipe.height)
+        if (collideWidth and (collideTop or collideBottom)):
             return True
         return False
 
@@ -104,26 +93,32 @@ class Bird:
 class FlappyBird:
     def __init__(self, graphique=True, FPS=30):
         # Setting up initial values
-        self.bird = Bird((windowObj.get_width() / 4 , windowObj.get_height() / 2))
-        self.pipes = [ Pipes()]
+        pygame.init()
+        self.windowObj = pygame.display.set_mode((640, 480))
+        self.fpsTimer = pygame.time.Clock()
+
+        self.bird = Bird((self.windowObj.get_width() / 4, self.windowObj.get_height() / 2))
+        self.pipes = [Pipes()]
         self.gravity = 2
         self.velocity = 0
         self.score = 0
         self.highScore = 0
         self.graphique = graphique
         self.FPS = FPS
-    
+
+        self.fontObj = pygame.font.Font(None, 16)
+
     def getState(self):
         if len(self.pipes) == 1:
             premierPipeHeight = (self.pipes[0].height - 210) / 390
             premierPipePos = self.pipes[0].pos / 600
-            
+
             deuxiemePipeHeight = 1
             deuxiemePipePos = 1
 
         else:
             compteur = 0
-            while self.pipes[compteur].pos <= 160 :
+            while self.pipes[compteur].pos <= 160:
                 compteur += 1
             premierPipeHeight = (self.pipes[compteur - 1].height - 210) / 390
             premierPipePos = self.pipes[compteur - 1].pos / 600
@@ -136,15 +131,15 @@ class FlappyBird:
 
     def getScore(self):
         return self.score
-        
+
     # Called to reset the game when you lose
     def resetGame(self):
-        if ( self.score > self.highScore ):
+        if (self.score > self.highScore):
             self.highScore = self.score
         self.score = 0
         self.velocity = 0
-        self.pipes = [ Pipes() ]
-        self.bird.pos = ( (windowObj.get_width() / 4 , windowObj.get_height() / 2) )
+        self.pipes = [Pipes()]
+        self.bird.pos = ((self.windowObj.get_width() / 4, self.windowObj.get_height() / 2))
 
     def pause(self):
         while True:
@@ -153,16 +148,16 @@ class FlappyBird:
                     pygame.quit()
                     sys.exit()
                 elif event.type == KEYDOWN:
-                    if ( event.key == K_ESCAPE):
+                    if (event.key == K_ESCAPE):
                         return
 
     def exit(self):
         pygame.quit()
         sys.exit()
-    
+
     def nextFrame(self, manual=False, entry=None):
         hasNotLost = True
-        windowObj.fill(backgroundColor)
+        self.windowObj.fill(backgroundColor)
 
         if not manual:
             # Check for events
@@ -171,7 +166,7 @@ class FlappyBird:
                     pygame.quit()
                     sys.exit()
                 elif event.type == KEYDOWN:
-                    if ( event.key == K_ESCAPE):
+                    if (event.key == K_ESCAPE):
                         self.pause()
                     # If the player hits a key, set velocity upward
                     self.velocity = -20
@@ -190,33 +185,35 @@ class FlappyBird:
             self.resetGame()
             self.velocity = 0
         for pipe in self.pipes:
-            if not pipe.replaced and pipe.pos < windowObj.get_width() / 2  :
+            if not pipe.replaced and pipe.pos < self.windowObj.get_width() / 2:
                 self.pipes[len(self.pipes):] = [Pipes()]
                 pipe.replaced = True
-            pipe.draw(windowObj)
+            pipe.draw(self.windowObj)
             if (self.bird.collision(pipe)):
-                windowObj.fill(pygame.Color('#230056'))
+                self.windowObj.fill(pygame.Color('#230056'))
                 self.resetGame()
-            if ( not pipe.scored and pipe.pos + pipe.width < self.bird.pos[0] ):
+            if (not pipe.scored and pipe.pos + pipe.width < self.bird.pos[0]):
                 self.score += 1
                 pipe.scored = True
-            if( not pipe.move(-10)):
+            if(not pipe.move(-10)):
                 del pipe
 
         # Draw stuff
-        scoreSurface = fontObj.render( 'Score: ' + str(self.score) + ' High: ' + str(self.highScore), False, fontColor)
+        scoreSurface = self.fontObj.render('Score: ' + str(self.score) + ' High: ' + str(self.highScore),
+                                           False, fontColor)
         scoreRect = scoreSurface.get_rect()
-        scoreRect.topleft = (windowObj.get_height() / 2 , 10)
-        windowObj.blit(scoreSurface, scoreRect)
-        pygame.draw.rect(windowObj, groundColor, (0, groundLevel, windowObj.get_width(), windowObj.get_height()) )
+        scoreRect.topleft = (self.windowObj.get_height() / 2, 10)
+        self.windowObj.blit(scoreSurface, scoreRect)
+        pygame.draw.rect(self.windowObj, groundColor, (0, groundLevel, self.windowObj.get_width(),
+                                                       self.windowObj.get_height()))
 
-        self.bird.draw(windowObj)
+        self.bird.draw(self.windowObj)
 
         if self.graphique:
             pygame.display.update()
-        fpsTimer.tick(self.FPS)
+        self.fpsTimer.tick(self.FPS)
         return hasNotLost
- 
+
 
 if __name__ == "__main__":
     flappy = FlappyBird()
