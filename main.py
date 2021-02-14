1a748032b1758b55de625d56f6c8b4d7d60043ba
Loïc Audoin <loic.audoin@supelec.fr>
2021-02-14 15:40:27 +0100

    main update

--- main.py
+++ main.py
@@ -1,10 +1,72 @@
-from flappyBird import FlappyBird
+from flappy import FlappyBird
+import keras
+from keras import layers
+import numpy as np
+from keras import models
+import time
+import tensorflow as tf
 
-if __file__== "__main__":
-    flappy = FlappyBird()
-    while True:
-        state = flappy.getState()
-        score = flappy.getScore()
 
-        entry = fonctionTropIntelligente(state)
-        flappy.nextFrame(manual=True, entry=entry)
+#%% Deep Q-Learning
+
+def alea(n,a,p):
+    r = np.random.random()
+    if r < p:
+        return np.random.randint(0,n)
+    else :
+        return a
+    
+def act(nSt,nSc,nP,mlp,state,score,p):
+    state = state.reshape((1,len(state)))
+    nSt = np.append(nSt,state, axis = 0)
+    nSc = np.append(nSc,score)
+    res = mlp.predict(state)[0]
+    n_action = len(res)
+    action_true = np.argmax(res)
+    action = alea(n_action,action_true,p)
+    nP = np.append(nP,res[action])
+    return action,nSt,nSc,nP
+
+
+
+def add(X,val):
+    n = len(X)
+    i = np.random.randint(0,n)
+    X[i] = val
+    return X 
+
+
+#%% Main 
+
+state_0 = np.zeros(6)
+
+nState = np.array([state_0])
+nScore = np.array([0])
+nPred = np.array(0)
+
+input_m = keras.Input(shape=(6,)) 
+x = layers.Dense(40, activation='tanh')(input_m)
+x = layers.Dense(20, activation='tanh')(x)
+x = layers.Dense(2, activation='tanh')(x)
+mlp = keras.Model(input_m,x)
+mlp.summary()
+mlp.compile(optimizer='adam', loss='binary_crossentropy')
+mlp.save('./mlp.h5')
+
+
+memoire = 10000
+X = [[] for i in range(memoire)]
+y = [-1 for i in range(memoire)]
+
+
+flappy = FlappyBird()
+while True:
+    state = np.array(flappy.getState())
+    score = flappy.getScore()
+    p = 1
+    action,nState,nScore,nPred = act(nState,nScore,nPred,mlp,state,score,p)
+    if action == 1:
+        entry = "jump"
+    else :
+        entry = "stay"
+    flappy.nextFrame(manual=True, entry=entry)
